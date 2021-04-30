bf738c78103bd3b154fc2181382ce540ff77b4fd
KazamiMichiru <matthewdm0816@gmail.com>
2021-04-30 21:52:28 +0800

    added simple yaml test

--- gcontrast.py
+++ gcontrast.py
@@ -20,4 +20,37 @@ from torch_geometric.utils import degree, get_laplacian, remove_self_loops
 from torch_geometric.nn import GATConv, knn_graph
 import torch_scatter as tscatter
 from icecream import ic
+from pprint import pprint
+
+from tensorboardX import SummaryWriter
+import time, random, os, sys, gc, copy, colorama, json, re
+from tabulate import tabulate
+
+import torch
+import torch.optim as optim
+from utils import *
+
+# limit CPU usage
+torch.set_num_threads(16)
+torch.set_num_interop_threads(16)
+print(
+    colorama.Fore.GREEN
+    + "Using %d/%d cores/threads of CPU"
+    % (torch.get_num_threads(), torch.get_num_interop_threads())
+)
+
+config_file = "config.yml"
+config = YAMLParser('config_file').data
+
+# ================ Config Tests
+assert config.optimizer_type in ["Adam", "SGD"]
+assert config.dataset_type in ["Cora", "Citeseer", "Pubmed"]
+config.ngpu = len(config.gpu_ids)
+config.parallel = config.ngpu > 1
+config.batch_size = config.batch_size_single * config.ngpu
+tabulate(config.items)
+
+device = torch.device("cuda:%d" % config.gpu_id if torch.cuda.is_available() else "cpu")
+
+# ===============
 
--- utils.py
+++ utils.py
@@ -1,10 +1,19 @@
-"""
+r"""
 General Helpers/Utilities
 """
-
+from yaml import load, dump
+from icecream import ic
+from tqdm import tqdm, trange 
+import pretty_errors
+from contextlib import contextmanager
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    ic()
+    from yaml import Loader, Dumper
 
 class layers:
-    """
+    r"""
     Enumerate layer sizes
     """
     def __init__(self, ns):
@@ -18,3 +27,28 @@ class layers:
 
     def __next__(self):
         return (next(self.iter1), next(self.iter2))
+
+
+class ObjectDict(dict):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def __getattr__(self, key):
+        return self[key]
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+class YAMLParser:
+    def __init__(self, filename):
+        with open(filename, 'r') as f:
+            self._data = load(f, Loader=Loader)
+        self.data = ObjectDict(self.data)
+
+    @property
+    def data(self):
+        return self._data
+
+    @contextmanager
+    def fetch(self):
+        yield self.data
